74b35bfc6ed1289b927bdc8cd0d85c3ab4fa2395
beansdrawer <beansdrawer@naver.com>
2024-04-26 11:42:30 +0900

    1부 완료

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -2,6 +2,7 @@ from apps.app import db
 from apps.crud.models import User
 from flask import Blueprint, render_template, redirect, url_for
 from apps.crud.forms import UserForm
+from flask_login import login_required
 
 crud = Blueprint(
     "crud",
@@ -11,10 +12,12 @@ crud = Blueprint(
 )
 
 @crud.route("/")
+@login_required
 def index():
     return render_template("crud/index.html")
 
 @crud.route("/sql")
+@login_required
 def sql():
     #####
 
@@ -22,6 +25,7 @@ def sql():
 
 
 @crud.route("/users/new", methods=["GET", "POST"])
+@login_required
 def create_user() :
     form = UserForm()
 
@@ -39,11 +43,13 @@ def create_user() :
     return render_template("crud/create.html", form=form)
 
 @crud.route("/users")
+@login_required
 def users() :
     users = User.query.all()
     return render_template("crud/index.html", users=users)
 
 @crud.route("/users/<user_id>", methods=["GET", "POST"])
+@login_required
 def edit_user(user_id):
     form = UserForm()
 
@@ -60,6 +66,7 @@ def edit_user(user_id):
     return render_template("crud/edit.html", user=user, form=form)
 
 @crud.route("/users/<user_id>/delete", methods=["POST"])
+@login_required
 def delete_user(user_id):
     user = User.query.filter_by(id=user_id).first()
     db.session.delete(user)
